ac34f7d809e3cb8cc081104f195ecf983a9629f1
cauc_xiao <1090822794@qq.com>
2023-06-21 13:01:34 +0800

    update

--- config/index.js
+++ config/index.js
@@ -5,11 +5,11 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://demo.open.renren.io/renren-fast-server';
+  window.SITE_CONFIG['baseUrl'] = 'http://localhost:88/api';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
-  window.SITE_CONFIG['version'] = '202306211231';   // 版本号(年月日时分)
+  window.SITE_CONFIG['version'] = '202306211300';   // 版本号(年月日时分)
   window.SITE_CONFIG['cdnUrl']  = window.SITE_CONFIG.domain + window.SITE_CONFIG.version;
 })();
 
